f22d2654564da9405551cf47915f6c9aa4960cb5
nick121212 <xiao.feng695@chinaredstar.com>
2017-09-25 23:33:22 +0800

    wp

--- packages/crawler.plugins.wp/src/plugins/wp.ts
+++ packages/crawler.plugins.wp/src/plugins/wp.ts
@@ -161,7 +161,7 @@ export class WpPlugin {
             "dwqa-question_category": category ? [category.id] : null,
             "dwqa-question_tag": tag ? [tag.id] : null,
             slug: config._id,
-            content: _.trim(resouce.content),
+            content: resouce.content,
             status: "publish",
             date: Moment().add(comments.length * 3 - 30, "day").format("YYYY-MM-DD hh:mm:ss"),
             ping_status: "open"
