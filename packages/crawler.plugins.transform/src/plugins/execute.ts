ef481d146803480991867d4f4f1cf4d94244c7f8
nick121212 <xiao.feng695@chinaredstar.com>
2017-09-25 13:03:44 +0800

    transform

--- packages/crawler.plugins.transform/src/plugins/execute.ts
+++ packages/crawler.plugins.transform/src/plugins/execute.ts
@@ -1,6 +1,6 @@
 import * as Seneca from "seneca";
 import inversify, { injectable, inject } from "inversify";
-import { Plugin, Add, Wrap, Init, Validate } from "crawler.plugins.common";
+import { Plugin, Add, Validate } from "crawler.plugins.common";
 import * as Joi from "joi";
 
 import { pluginName } from "../constants";
@@ -11,17 +11,15 @@ import { jsonata } from "../index";
 export class TransformExexutePlugin {
     /**
      * 启动一个任务
-     * @param param0 
-     * @param options 
-     * @param globalOptions 
+     * @param param0 数据
      */
     @Add(`role:${pluginName},cmd:single`)
 
     private async single(
         @Validate(Joi.object().keys({
-            expression: Joi.string().required(),
-            data: Joi.any().required()
-        }), { allowUnknown: true }) { expression, data }: { expression: string, data: any }, options?: any, globalOptions?: any) {
+            expression: Joi.string().required().label("表达式"),
+            data: Joi.any().required().label("数组字段")
+        }), { allowUnknown: true }) { expression, data }: { expression: string, data: any }) {
 
         let exp = jsonata(expression);
 
@@ -31,19 +29,17 @@ export class TransformExexutePlugin {
     }
     /**
      * 启动一个任务
-     * @param param0 
-     * @param options 
-     * @param globalOptions 
+     * @param param0 数据
      */
     @Add(`role:${pluginName},cmd:muti`)
     private async muti( @Validate(Joi.object().keys({
-        expressions: Joi.array().items(Joi.string().required()).required(),
-        data: Joi.any().required()
+        expressions: Joi.array().items(Joi.string().required().label("表达式")).required().label("表达式列表"),
+        data: Joi.any().required().label("数组字段")
     }), { allowUnknown: true }) { expressions, data }: { expressions: Array<string>, data: any }, options?: any, globalOptions?: any) {
         let rets: Array<any> = [];
 
         expressions.forEach(async (expression: string) => {
-            rets.push((await this.single({ expression, data }, options, globalOptions)).result);
+            rets.push((await this.single({ expression, data })).result);
         });
 
         return { result: rets };
--- packages/crawler.plugins.transform/src/start.ts
+++ packages/crawler.plugins.transform/src/start.ts
@@ -7,11 +7,6 @@ let seneca = new Seneca<any>(container, {
     tag: "crawler.plugins.transform"
 });
 
-
 seneca.seneca.ready(async () => {
     console.log("crawler.plugins.transform ready!");
-
-    // let a = await seneca.seneca.actAsync("role:crawler.plugin.transform,cmd:single", { expression: "$sum($.*)", data: { a: 1,b:2,c:4 } })
-    // let abc = await seneca.seneca.actAsync("role:crawler.plugin.transform,cmd:muti", { expressions: ["a", "b", "c"], data: { a: 1, b: 2, c: 3 } })
-    // console.log(a, abc);
 });
