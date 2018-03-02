82d9b96886efc1fa73ec2a9e52aa0b9c96df33d0
nick121212 <nick121212@126.com>
2018-03-02 14:04:24 +0800

    豆瓣的配置文件

--- packages/crawler.plugins.schedule/src/config/douban/detail.ts
+++ packages/crawler.plugins.schedule/src/config/douban/detail.ts
@@ -41,7 +41,7 @@ export default {
             "save": false,
             "engine": "superagent",
             "header": {
-                "Cookie": "bid=hNu_TjLIVX0; __yadk_uid=bzXm0KNbXIgPPn8HSYqFvApFNR8wDedU; ll=\"108296\"; _vwo_uuid_v2=49098C96E44F149C32B802A921D721AF|c8ade32f54b25436b272c029c4a65b5e; push_noty_num=0; push_doumail_num=0; __utmv=30149280.16924; ap=1; dbcl2=\"169248639:g9iNSRdktEk\"; ck=Gze-; __utmc=30149280; __utmz=30149280.1513844581.9.4.utmcsr=baidu|utmccn=(organic)|utmcmd=organic; _pk_ref.100001.8cb4=%5B%22%22%2C%22%22%2C1514352705%2C%22https%3A%2F%2Fwww.baidu.com%2Flink%3Furl%3D5CjIAS_tB6rdw9GzDbooRliM24AtVP4J789ku3qiT67b4vemnstufGwkkmRoWm8GIy8Qy7Hvs01letueQEs3f_%26wd%3D%26eqid%3D98eb35ec000110b2000000045a3b6df0%22%5D; _pk_id.100001.8cb4=5eda58f6467f1643.1508223798.12.1514352705.1514276808.; _pk_ses.100001.8cb4=*; __utma=30149280.90236051.1508223798.1514276706.1514352706.11; __utmt=1; __utmb=30149280.6.6.1514352706"
+                "Cookie": "dbcl2=\"174606656:YW2RDyjCUvQ\";"
             }
         },
         "result": "${'queueItem':$}"
@@ -84,10 +84,10 @@ export default {
         }
     }, {
         "partten": "role:crawler.plugin.transform,cmd:single",
-        "jsonata": ["${'data':$$.result.images}"],
-        "condition": "!$not($$.result.images)",
+        "jsonata": ["$.{'data':$$.result.images}"],
+        "condition":"$boolean($$.result.images)",
         "data": {
-            "expression": "${'images':$$.result.images.$join('--------')}"
+            "expression": "${'images':$join($$,'--------')}"
         },
         "result": "$"
     }, {
@@ -95,7 +95,7 @@ export default {
         "title": "把解析到的数据存入到mq中",
         "jsonata": ["$.{'items':[$.result]}"],
         "data": {
-            "key": "doubanList",
+            "key": "doubanDetail",
             "routingKey": "crawler.url.lait.tv.result"
         }
     }]
